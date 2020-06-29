1e00f4ca372c20abf992b1092a76cad0f363d138
Maurice522 <67146519+Maurice522@users.noreply.github.com>
2020-06-29 07:54:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var random1= Math.floor(Math.random()*6)+1;
+var randomimg1="images/dice"+ random1 +".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomimg1);
+
+var random2= Math.floor(Math.random()*6)+1;
+var randomimg2="images/dice"+ random2 +".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomimg2);
+
+if(random1>random2)
+{
+  document.querySelector("h1").innerHTML = "Maurice wins 😛";
+}
+else if(random2>random1)
+{
+  document.querySelector("h1").innerHTML = "you win 😪";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw, Maurice wins 🙈";
+}
